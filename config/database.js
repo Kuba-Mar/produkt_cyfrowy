ab1ed0227b92451431ff01f6679924662d4551f4
Kuba-Mar <Kuba-Mar@users.noreply.github.com>
2024-09-12 20:17:33 +0200

    Dodanie pliku konfiguracyjnego

--- config/database.js
+++ config/database.js
@@ -1,12 +1,29 @@
 // config/database.js
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 // Upewnij się, że te parametry są poprawne
-const sequelize = new Sequelize('moja_baza_danych', 'uzytkownik', 'haslo', {
-  host: 'localhost', // lub inny adres, jeśli baza danych jest na innym serwerze
-  port: 5432, // domyślny port PostgreSQL
+// const sequelize = new Sequelize('moja_baza_danych', 'uzytkownik', 'haslo', {
+//   host: 'localhost', // lub inny adres, jeśli baza danych jest na innym serwerze
+//   port: 5432, // domyślny port PostgreSQL
+//   dialect: 'postgres',
+//   logging: false, // Wyłącz logowanie, aby zredukować wyjścia w konsoli
+// });
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'postgres',
-  logging: false, // Wyłącz logowanie, aby zredukować wyjścia w konsoli
+  logging: false,
 });
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Połączenie z bazą danych zostało nawiązane.');
+  })
+  .catch(err => {
+    console.error('Błąd podczas łączenia z bazą danych:', err);
+  });
+
 
 module.exports = sequelize;
